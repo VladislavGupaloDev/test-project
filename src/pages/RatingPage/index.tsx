d59802a89eddf2fc67ceb9acb4dec29ee8966b5a
Vladislav Gupalo <parallaxtrashbox@gmail.com>
2025-05-16 12:15:16 +0500

    fix(ci): add meta env fro fetching public file

--- src/pages/RatingPage/index.tsx
+++ src/pages/RatingPage/index.tsx
@@ -25,7 +25,7 @@ export function RatingPage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/src/db.json')
+        const response = await fetch(import.meta.env.BASE_URL + 'db.json')
         dispatch(setRateData(await response.json()))
       } catch (error) {
         console.error(error)
